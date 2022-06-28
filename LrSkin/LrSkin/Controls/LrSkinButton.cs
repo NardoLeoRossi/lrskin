f113970b436873c5abfb16bb9a86d7cb8c50081c
Leonardo Rossi <rossi_leo@outlook.com>
2022-06-27 23:30:42 -0300

    Criando o projeto

---
+++ LrSkin/LrSkin/Controls/LrSkinButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LrSkin.Controls
+{
+    public class LrSkinButton: Button
+    {
+        private int _edgeRadius;
+
+        /// <summary>
+        /// Representa o raio da borda do controle.
+        /// </summary>
+        [DefaultValue(true)]
+        [Category("LR Skin")]
+        public int EdgeRadius { get; set; }
+
+
+
+
+    }
+}
