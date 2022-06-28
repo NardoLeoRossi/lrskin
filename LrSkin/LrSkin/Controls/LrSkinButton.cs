80c6ea3e0967d745a2be1c3a21f40200443bf506
Leonardo Rossi <rossi_leo@outlook.com>
2022-06-27 23:57:11 -0300

    iniciando LrSkinButton

--- LrSkin/LrSkin/Controls/LrSkinButton.cs
+++ LrSkin/LrSkin/Controls/LrSkinButton.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using LrSkin.Helpers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,7 @@ namespace LrSkin.Controls
 {
     public class LrSkinButton: Button
     {
+        #region Properties
         private float _edgeRadius;
         [DefaultValue(4), Category("LR Skin"), Description("Representa o raio da borda do controle")]
         public float EdgeRadius 
@@ -21,6 +26,33 @@ namespace LrSkin.Controls
                 Invalidate();
             }
         }
+
+        #endregion
+
+
+
+        #region Events
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            var g = pevent.Graphics;
+
+            g.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.Clear(Parent.BackColor);
+
+            //Retangulo base
+            RectangleF buttonRectF = new RectangleF(ClientRectangle.Location, ClientRectangle.Size);
+            buttonRectF.X -= 0.5f;
+            buttonRectF.Y -= 0.5f;
+            GraphicsPath buttonPath = DrawHelper.GetRoundRect(buttonRectF, 4);
+
+    
+
+
+            base.OnPaint(pevent);
+        }
+
+        #endregion
 
 
 
